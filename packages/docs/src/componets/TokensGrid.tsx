53ad78296274d3954e4077731f2684b777d9bb8e
Edson Rodrigo <edson.rodrigo@liveonsolutions.com>
2023-10-23 19:23:11 -0300

    add all token files in docs by components

--- packages/docs/src/componets/TokensGrid.tsx
+++ packages/docs/src/componets/TokensGrid.tsx
@@ -1,17 +1,29 @@
+import '../styles/tokens-grid.css'
+
 interface TokensGridProd {
   tokens: Record<string, string>
+  hasRemValue?: boolean
 }
 
-export function TokensGrid({ tokens }: TokensGridProd) {
+export function TokensGrid({ tokens, hasRemValue = false }: TokensGridProd) {
   return (
-    <table>
+    <table className="tokens-grid">
       <thead>
         <tr>
           <th>Name</th>
           <th>Value</th>
+          {hasRemValue && <th>Pixels</th>}
         </tr>
       </thead>
-      <tbody></tbody>
+      <tbody>
+        {Object.entries(tokens).map(([key, value]) => (
+          <tr key={key}>
+            <td>{key}</td>
+            <td>{value}</td>
+            {hasRemValue && <td>{+value.replace('rem', '') * 16}px</td>}
+          </tr>
+        ))}
+      </tbody>
     </table>
   )
 }
