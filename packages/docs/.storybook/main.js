092e2fb4822b47479912485e6d2e2580c31c8d9c
Edson Rodrigo <edson.rodrigo@liveonsolutions.com>
2023-10-26 11:37:44 -0300

    fix: add base path in sb to deploy

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -35,5 +35,11 @@ const config = {
     autodocs: true,
     defaultName: 'Documentation',
   },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/design-system-ignite/'
+    }
+    return config
+  }
 };
 export default config;
